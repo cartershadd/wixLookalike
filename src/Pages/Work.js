e6909ead160acd894498e5c9e40790c9c686c667
Carter Shadden <carter.shadden@gmail.com>
2020-01-09 14:30:13 -0500

    fixed overlap and sizing issues on mobile and fullscreen

--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -19,18 +19,11 @@ class Work extends Component {
                 <div className="holder-div">
                     <Card className="work">
                         <Card.Title>Work</Card.Title>
-                        <Card.Img variant="top" src="rainbowCake.jpeg/" />
+                        <Card.Img variant="top" src="/images/rainbowCake.jpeg"/>
                         <Card.Body>
-                            <Card.Text style={{margin: '0.25rem'}}>
+                            <Card.Text>
                                 Sesame snaps croissant dragée. Muffin I love chocolate I love pie I love. Candy canes lollipop I love icing marzipan bear claw jujubes.
-
-                                Gingerbread soufflé donut danish chocolate cake cake donut gingerbread marshmallow. Fruitcake soufflé I love candy. Chocolate bar jelly beans cheesecake chocolate bar chupa chups. Oat cake lemon drops lemon drops sweet I love biscuit lemon drops jujubes.
-
-                                Tart sugar plum ice cream toffee chupa chups danish bear claw cheesecake candy canes. Halvah danish biscuit. Chupa chups gummies lemon drops.
-
-                                Tart pastry sweet I love cake I love cupcake pastry. Marzipan pie fruitcake sugar plum pie liquorice marshmallow. I love croissant sweet roll I love. Jujubes liquorice muffin cake.
-
-                                Macaroon gummies cookie cupcake. Jelly-o I love tootsie roll marshmallow I love chocolate bar lollipop soufflé. Cake apple pie chupa chups jujubes carrot cake halvah soufflé.
+                                Gingerbread soufflé donut.
                             </Card.Text>
                         </Card.Body>
                         <Button variant="primary" className="back-to-top" onClick={this.componentDidMount}>Back to Top</Button>
