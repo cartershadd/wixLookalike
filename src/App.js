8149dd4caa87acf4d5ded6327c2183ca2caa8216
Carter Shadden <carter.shadden@gmail.com>
2020-01-13 10:05:36 -0500

    removed route and browser router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter} from "react-router-dom";
 import NavPage from "./Navigation/navPage";
-import Route from "react-router-dom/es/Route";
 import Container from "./Pages/Container";
 
 function App() {
     return (
-        <BrowserRouter>
             <div className="app">
                 <NavPage/>
-                <Route path="/" component={Container}/>
+                <Container/>
             </div>
-        </BrowserRouter>
     )
 }
 
